38e4849a8b85832381ae354325ca42e519c6e714
GetCreativeMalhi <108103415+GetCreativeMalhi@users.noreply.github.com>
2026-02-11 12:49:38 +1100

    feat: display new books on homepage

--- function.js
+++ function.js
@@ -25,4 +25,65 @@ function addBookToLibrary(id, title, author, pages, readStatus){
         
 }
 
-addBookToLibrary(crypto.randomUUID(), 'The Hobbit', 'J. R. R. Tolkien', 295, 'not read');
+function readArrayDisplayBooks(array){
+    var rObjTitle = "";
+    var rObjAuthor = "";
+    var rObjPages = "";
+    var rObjStatus = "";
+    const contentGrid = document.querySelector(".content-grid");
+
+    for(const bookObj of myLibrary){
+        rObjAuthor = bookObj.author;
+        rObjTitle = bookObj.title;
+        rObjPages = bookObj.pages;
+        rObjStatus = bookObj.readStatus;
+
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        const cardTitle = document.createElement("h3");
+        const cardAuthor = document.createElement("h4");
+        const cardPages = document.createElement("h4");
+        const cardStatus = document.createElement("h4");
+
+
+        const cardAuthorSpan = document.createElement("span");
+        const cardPagesSpan = document.createElement("span");
+        const cardStatusSpan = document.createElement("span");
+
+        cardAuthorSpan.classList.add("muted");
+        cardPagesSpan.classList.add("muted");
+        cardStatusSpan.classList.add("muted");
+
+        cardAuthorSpan.textContent = rObjAuthor;
+        cardPagesSpan.textContent = rObjPages;
+        cardStatusSpan.textContent = rObjStatus;
+        cardTitle.textContent = rObjTitle;
+        cardAuthor.textContent = "Author: ";
+        cardPages.textContent = "Pages: ";
+        cardStatus.textContent = "Status: ";
+
+        cardAuthor.appendChild(cardAuthorSpan);
+        cardPages.appendChild(cardPagesSpan);
+        cardStatus.appendChild(cardStatusSpan);
+
+        card.appendChild(cardTitle);
+        card.appendChild(cardAuthor);
+        card.appendChild(cardPages);
+        card.appendChild(cardStatus);
+
+        contentGrid.appendChild(card);
+    }
+
+}; 
+
+addBookToLibrary(crypto.randomUUID(), 'The Hobbit', 'J. R. R. Tolkien', 295, 'Not read');
+addBookToLibrary(crypto.randomUUID(), 'The Hobbit', 'J. R. R. Tolkien', 295, 'Not read');
+addBookToLibrary(crypto.randomUUID(), 'The Hobbit', 'J. R. R. Tolkien', 295, 'Not read');
+addBookToLibrary(crypto.randomUUID(), 'The Hobbit', 'J. R. R. Tolkien', 295, 'Not read');
+addBookToLibrary(crypto.randomUUID(), 'The Hobbit', 'J. R. R. Tolkien', 295, 'Not read');
+addBookToLibrary(crypto.randomUUID(), 'The Hobbit', 'J. R. R. Tolkien', 295, 'Not read');
+addBookToLibrary(crypto.randomUUID(), 'The Hobbit', 'J. R. R. Tolkien', 295, 'Not read');
+addBookToLibrary(crypto.randomUUID(), 'The Hobbit', 'J. R. R. Tolkien', 295, 'Not read');
+addBookToLibrary(crypto.randomUUID(), 'The Hobbit', 'J. R. R. Tolkien', 295, 'Not read');
+readArrayDisplayBooks(myLibrary);
